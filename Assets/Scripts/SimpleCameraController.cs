9e5e8740c6fc53b8ae592eb6942fe60351bc97ee
Arahyn <thomas.ducray@hotmail.fr>
2020-03-23 17:10:50 +0100

    Importation du package VFX

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -79,7 +79,7 @@ namespace UnityTemplateProjects
         Vector3 GetInputTranslationDirection()
         {
             Vector3 direction = new Vector3();
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.Z))
             {
                 direction += Vector3.forward;
             }
@@ -87,7 +87,7 @@ namespace UnityTemplateProjects
             {
                 direction += Vector3.back;
             }
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.Q))
             {
                 direction += Vector3.left;
             }
@@ -95,7 +95,7 @@ namespace UnityTemplateProjects
             {
                 direction += Vector3.right;
             }
-            if (Input.GetKey(KeyCode.Q))
+            if (Input.GetKey(KeyCode.A))
             {
                 direction += Vector3.down;
             }
@@ -151,7 +151,7 @@ namespace UnityTemplateProjects
             }
             
             // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
-            boost += Input.mouseScrollDelta.y * 0.2f;
+            //boost += Input.mouseScrollDelta.y * 0.2f;
             translation *= Mathf.Pow(2.0f, boost);
 
             m_TargetCameraState.Translate(translation);
