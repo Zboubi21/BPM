ab0542dff2534c9c7943b8788b486cb0fd888962
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-03-26 16:52:09 +0100

    Add test anim weapon Try to change weapon bpm value shader with TestChangeShaderValue.cs

---
+++ Assets/3D/Weapon/TestSwitchAninm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSwitchAninm : MonoBehaviour
+{
+    
+    [SerializeField] KeyCode m_debugInput = KeyCode.M;
+
+    Animator m_animator;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(m_debugInput) && m_animator != null)
+        {
+            m_animator.SetTrigger("NextAnim");
+        }
+    }
+    
+}
