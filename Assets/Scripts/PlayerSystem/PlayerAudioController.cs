aa3728af42861b0de3672760c5f4365e4af06429
Zoulouti <Pauldelaubadere@gmail.com>
2020-03-31 11:48:08 +0200

    push pour gaultier

--- Assets/Scripts/PlayerSystem/PlayerAudioController.cs
+++ Assets/Scripts/PlayerSystem/PlayerAudioController.cs
@@ -243,7 +243,6 @@ public class PlayerAudioController : AudioController
     {
         if (weaponSound.allDifferentUpgradeSound.Length == (int)BPMSystem.WeaponState.Fury && weaponSound.morphSource != null)
         {
-            Debug.Log("levelToUpgrade " + levelToUpgrade);
             StartSoundFromArray(weaponSound.fireSource, weaponSound.allDifferentFireSound[levelToUpgrade].allDifferentClip, weaponSound.allDifferentLastFireSound[levelToUpgrade].Volume.volume, weaponSound.allDifferentLastFireSound[levelToUpgrade].Volume.volumeRandomizer, weaponSound.allDifferentLastFireSound[levelToUpgrade].Pitch.pitch, weaponSound.allDifferentLastFireSound[levelToUpgrade].Pitch.pitchRandomizer);
         }
     }
@@ -252,7 +251,6 @@ public class PlayerAudioController : AudioController
     {
         if (weaponSound.allDifferentDegradeSound.Length == (int)BPMSystem.WeaponState.Fury && weaponSound.morphSource != null)
         {
-            Debug.Log("degradeFrom " + degradeFrom);
             StartSoundFromArray(weaponSound.fireSource, weaponSound.allDifferentFireSound[degradeFrom].allDifferentClip, weaponSound.allDifferentLastFireSound[degradeFrom].Volume.volume, weaponSound.allDifferentLastFireSound[degradeFrom].Volume.volumeRandomizer, weaponSound.allDifferentLastFireSound[degradeFrom].Pitch.pitch, weaponSound.allDifferentLastFireSound[degradeFrom].Pitch.pitchRandomizer);
         }
     }
