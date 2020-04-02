e7d19330fbbbb46c1ec7fb91a8cc18c37aa5ec28
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-04-02 16:56:48 +0200

    Start to create the PlayerDamageIndicator in BPMSystem.cs

--- Assets/Scripts/PlayerSystem/BPMSystem.cs
+++ Assets/Scripts/PlayerSystem/BPMSystem.cs
@@ -76,6 +76,14 @@ public class BPMSystem : MonoBehaviour
         public Image _overdrenalineButton;
 
     }
+
+    [SerializeField] DamageIndicator m_damageIndicator;
+    [Serializable] class DamageIndicator
+    {
+        public Transform m_target;
+        public RectTransform m_canvasTrans;
+    }
+    
     float _currentOverdrenalineCooldown;
     bool _canUseFury;
     bool _furyCoolDownOver;
@@ -84,6 +92,7 @@ public class BPMSystem : MonoBehaviour
     PlayerController m_playerController;
     WeaponBehaviour weapon;
     PlayerAudioController audioControl;
+
     private void Start()
     {
         m_playerController = GetComponent<PlayerController>();
@@ -116,6 +125,10 @@ public class BPMSystem : MonoBehaviour
     void FixedUpdate()
     {
         SetBpmGaugeShader();
+    }
+    void LateUpdate()
+    {
+        TestRotation();
     }
 
     #region BPM Gain and Loss
@@ -374,6 +387,20 @@ public class BPMSystem : MonoBehaviour
     bool HasUsedFury()
     {
         return (_canUseFury && Input.GetButtonDown("OverAdrenaline") && _furyCoolDownOver);
+    }
+
+    void TestRotation()
+    {
+        if (m_damageIndicator.m_target == null)
+            return;
+        
+        Vector3 direction = m_playerController.m_references.m_cameraPivot.position - m_damageIndicator.m_target.position;
+        Quaternion tRot = Quaternion.LookRotation(direction);
+        tRot.z = -tRot.y;
+        tRot.x = 0;
+        tRot.y = 0;
+        Vector3 nortDirection = new Vector3(0, 0, m_playerController.m_references.m_cameraPivot.eulerAngles.y);
+        m_damageIndicator.m_canvasTrans.localRotation = tRot * Quaternion.Euler(nortDirection);
     }
     #endregion
 }
