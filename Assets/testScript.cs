7fdbeeb1438a17f1c641d3c641285527c2de2cc0
Zoulouti <Pauldelaubadere@gmail.com>
2020-04-02 12:13:14 +0200

    tooltip pour hugo et quentin

--- Assets/testScript.cs
+++ Assets/testScript.cs
@@ -9,10 +9,47 @@ public class testScript : MonoBehaviour
 
     private float timeCount = 0.0f;
 
+    //void Update()
+    //{
+    //    transform.position = 
+    //    transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
+    //    
+    //}
+    public float time;
+    float _currentTime;
+    public Transform target;
+    bool go;
+    Vector3 relativePos;
+    Vector3 initForward;
     void Update()
     {
-        transform.position = Vector3.Slerp(from.position, to.position, timeCount);
-        transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
-        timeCount = timeCount + Time.deltaTime;
+
+        if ((Input.GetKeyDown(KeyCode.Alpha8) || go) && timeCount <= 1)
+        {
+            go = true;
+            LetsGo();
+        }
+        else
+        {
+            go = false;
+            timeCount = 0;
+            _currentTime = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            relativePos = target.position - transform.position;
+            initForward = transform.forward;
+        }
+
+    }
+
+    void LetsGo()
+    { 
+        _currentTime += Time.deltaTime;
+        timeCount = Mathf.InverseLerp(0, time, _currentTime);
+        Quaternion rotation = Quaternion.LookRotation(Vector3.Slerp(initForward, relativePos, timeCount));
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, rotation.eulerAngles.y, transform.eulerAngles.z);
     }
 }
