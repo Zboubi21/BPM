cd4aeb942b2527d17e3ca18ce0eff6c726599099
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-04-16 10:28:40 +0200

    Little break force upgrade on BreakableObjectController.cs

--- Assets/Scripts/DestroyableObject/BreakableObjectController.cs
+++ Assets/Scripts/DestroyableObject/BreakableObjectController.cs
@@ -8,6 +8,7 @@ public class BreakableObjectController : DestroyableObjectController
     [SerializeField] GameObject m_baseMesh;
     [SerializeField] GameObject m_breakMesh;
     [SerializeField] float m_breakForce = 3;
+    [SerializeField] float m_upForce = 3;
     
     protected override void On_ObjectIsBreak()
     {
@@ -16,7 +17,17 @@ public class BreakableObjectController : DestroyableObjectController
         Rigidbody[] rbody = GetComponentsInChildren<Rigidbody>();
         for (int i = 0, l = rbody.Length; i < l; ++i)
         {
-            rbody[i].AddForce(rbody[i].transform.position + (-rbody[i].transform.up * m_breakForce));
+            // rbody[i].AddForce(rbody[i].transform.position + (-rbody[i].transform.up * m_breakForce));
+            // rbody[i].AddForce(rbody[i].transform.position + (rbody[i].transform.up * m_breakForce));
+
+            Vector3 force = ((rbody[i].transform.position - transform.position).normalized * m_breakForce) + (rbody[i].transform.forward * m_upForce);
+            rbody[i].AddForce(force);
+
+            // int alea = Random.Range(0, 100);
+            // if (alea <= 50)
+            //     rbody[i].AddForce(rbody[i].transform.position + (transform.forward * m_breakForce));
+            // if (alea > 50)
+            //     rbody[i].AddForce(rbody[i].transform.position + (-transform.forward * m_breakForce));
         }
     }
 
