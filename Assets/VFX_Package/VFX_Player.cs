4617b0c3c0d1277f3e78184927269f1610525a42
Arahyn <thomas.ducray@hotmail.fr>
2020-04-28 10:31:01 +0200

    VFX + Wall Street Plugin

---
+++ Assets/VFX_Package/VFX_Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFX_Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            ParticleSystem[] VFXArray = GetComponentsInChildren<ParticleSystem>();
+            foreach (ParticleSystem VFX in VFXArray)
+            {
+                VFX.Play();
+            }
+        }
+    }
+}
