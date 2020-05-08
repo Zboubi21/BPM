23ecba49b3a5304e01feea9dfcd8362f254f26a9
Arahyn <thomas.ducray@hotmail.fr>
2020-05-08 17:37:10 +0200

    Tweak VFX ennemis

---
+++ Assets/VFX_Thomas/Plexus_Effect.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(ParticleSystem))]
+public class Plexus_Effect : MonoBehaviour
+{
+    // Creates a line renderer that follows a Sin() function
+    // and animates it.
+
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 20;
+
+    ParticleSystem m_System;
+    ParticleSystem.Particle[] m_Particles;
+    public float m_Drift = 0.01f;
+
+    void Start()
+    {
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = lengthOfLineRenderer;
+
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        lineRenderer.colorGradient = gradient;
+    }
+
+    void Update()
+    {
+        InitializeIfNeeded();
+
+        // GetParticles is allocation free because we reuse the m_Particles buffer between updates
+        int numParticlesAlive = m_System.GetParticles(m_Particles);
+
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        var t = Time.time;
+
+        // Change only the particles that are alive
+        for (int i = 0; i < numParticlesAlive; i++)
+        {
+            for (int k = 0; k < lengthOfLineRenderer; k++)
+            {
+                //lineRenderer.SetPosition(m_Particles[i].position, m_Particles[i].position);
+            }
+        }
+    }
+
+    void InitializeIfNeeded()
+    {
+        if (m_System == null)
+            m_System = GetComponent<ParticleSystem>();
+
+        if (m_Particles == null || m_Particles.Length < m_System.main.maxParticles)
+            m_Particles = new ParticleSystem.Particle[m_System.main.maxParticles];
+    }
+}
---
+++ Assets/VFX_Thomas/Shader_Trigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shader_Trigger : MonoBehaviour
+{
+    public ParticleSystem VFX;
+    public float start_Lerp_Value;
+    public float end_Lerp_Value;
+    public float Lerp_Duration;
+    float lerpValue;
+    Renderer renderer;
+
+    public string proterty_Name;
+
+    private float t = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lerpValue = start_Lerp_Value;
+        renderer = GetComponent<Renderer> ();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            t = 0;
+            VFX.Play();
+        }    
+
+        if (VFX.isPlaying)
+        {
+            lerpValue = Mathf.Lerp(start_Lerp_Value, end_Lerp_Value, t);
+            if (t < 1)
+            {
+                t += Time.deltaTime / Lerp_Duration;
+            }
+        }     
+
+        renderer.material.SetFloat(proterty_Name, lerpValue);
+    }
+}
