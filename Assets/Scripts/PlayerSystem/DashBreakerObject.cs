6269e4d23c5802bf3767a578fc78f19095843f83
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-05-15 16:30:43 +0200

    Start to create DashBreakerObject.cs

---
+++ Assets/Scripts/PlayerSystem/DashBreakerObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashBreakerObject : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider col)
+    {
+        // Debug.Log(col.gameObject.name);
+    }
+    
+}
