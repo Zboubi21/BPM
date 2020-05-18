aadefd2fb922659b70ce3baa1f9008cc159b7fa3
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-05-18 12:08:23 +0200

    Replace player muzzle flash

---
+++ Assets/Scripts/ShootOrigin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootOrigin : MonoBehaviour
+{
+    
+    [SerializeField] Transform m_targetTrans;
+    [SerializeField] float m_yLocalFreezePos;
+
+    void FixedUpdate()
+    {
+        transform.position = m_targetTrans.position;
+        transform.localPosition = new Vector3(transform.localPosition.x, m_yLocalFreezePos, transform.localPosition.z);
+    }
+    
+}
