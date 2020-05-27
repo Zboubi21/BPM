76c3d607ef1366fe2d1c386227df16a151e24ff8
JeremyPayre Laptop <jeremy.payre@live.fr>
2020-05-27 12:35:36 +0200

    Modify A_showcase_double_Prefab_.prefab

--- Assets/Scripts/DestroyableObject/BreakableObjectController.cs
+++ Assets/Scripts/DestroyableObject/BreakableObjectController.cs
@@ -7,6 +7,7 @@ public class BreakableObjectController : DestroyableObjectController
 
     [SerializeField] GameObject m_baseMesh;
     // [SerializeField] GameObject m_breakMesh;
+    [SerializeField] bool m_justeUseForwardForce = false;
     [SerializeField] float m_breakForce = 3;
     [SerializeField] float m_upForce = 3;
 
@@ -30,7 +31,7 @@ public class BreakableObjectController : DestroyableObjectController
             // rbody[i].AddForce(rbody[i].transform.position + (-rbody[i].transform.up * m_breakForce));
             // rbody[i].AddForce(rbody[i].transform.position + (rbody[i].transform.up * m_breakForce));
 
-            Vector3 force = ((rbody[i].transform.position - transform.position).normalized * m_breakForce) + (rbody[i].transform.forward * m_upForce);
+            Vector3 force = m_justeUseForwardForce ? rbody[i].transform.forward * m_breakForce : ((rbody[i].transform.position - transform.position).normalized * m_breakForce) + (rbody[i].transform.forward * m_upForce);
             rbody[i].AddForce(force);
 
             // int alea = Random.Range(0, 100);
